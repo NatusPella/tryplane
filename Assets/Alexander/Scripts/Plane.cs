19e9fad443ebee556ed23efa1cb7bc6c9c5b98de
Alexander Buist <a.buist@natuspella.nl>
2022-06-05 12:32:46 +0200

    Update like you've never updated before

--- Assets/Alexander/Scripts/Plane.cs
+++ Assets/Alexander/Scripts/Plane.cs
@@ -5,8 +5,6 @@ namespace Tryplane.Alexander
     public class Plane : MonoBehaviour
     {
         public GameObject destroyedPrefab;
-
-        private float forcedHorizontalSpeed = 5f;
         private float power = 25f;
         new private Rigidbody2D rigidbody2D;
 
@@ -15,14 +13,12 @@ namespace Tryplane.Alexander
         {
             rigidbody2D = GetComponent<Rigidbody2D>();
 
-            rigidbody2D.AddForce(Vector2.right * forcedHorizontalSpeed * 100f);
+            rigidbody2D.AddForce(Vector2.right * 1000f);
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
-            //rigidbody2D.AddForce(Vector2.right * forcedHorizontalSpeed);
-
             rigidbody2D.AddForce(Vector2.up * Input.GetAxis("Vertical") * power);
             rigidbody2D.AddForce(Vector2.right * Input.GetAxis("Horizontal") * power);
 
@@ -31,11 +27,6 @@ namespace Tryplane.Alexander
             {
                 float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            }
-
-            if (Input.GetButton("Boost"))
-            {
-                rigidbody2D.AddForce(transform.right * 100f);
             }
         }
 
@@ -46,11 +37,8 @@ namespace Tryplane.Alexander
                 return;
             }
 
-            if (rigidbody2D.velocity.y < -10f)
-            {
-                Instantiate(destroyedPrefab, transform.position, transform.rotation);
-                Destroy(gameObject);
-            }
+            Instantiate(destroyedPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
         }
 
     }
